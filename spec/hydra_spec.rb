b124d6afc48484d33e63d6c89d93fcc253d89e0a
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2016-03-30 23:06:31 +0100

    Cleaned up debugging statement.

--- spec/hydra_spec.rb
+++ spec/hydra_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 # TODO Spec out .dump
 describe Hydra do
   describe '.count' do
-    it "counts", focus: true do
+    it "counts" do
       hydra = Hydra.new
       hydra.ingest(['a', 'b', 'c'])
       expect(hydra.count).to eq 3
