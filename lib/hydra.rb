2b0583ef8f1c64c07927b4dd2b670057eba7e6c2
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2016-03-30 22:30:59 +0100

    Hydra ingest and count, initial implementation.

--- lib/hydra.rb
+++ lib/hydra.rb
@@ -1,8 +1,47 @@
+require 'byebug'
+require 'pp'
+
 class Hydra
+  def initialize
+    @root = { }
+  end
+
+  def self.count_node(node)
+    node.inject(0) do |sum, head|
+      head, tail = head.first, head.last
+      sum += 1 if head == 0
+      sum += if tail.is_a? Hash then Hydra.count_node(tail) else 0 end
+    end
+  end
+
   def count
-    return 3
+    Hydra.count_node(@root)
+  end
+
+  def self.ingest_word(node, word)
+    head = word[0]
+    if head
+      tail = word[1..-1]
+      node[head] ||= { }
+      if tail == ""
+        node[head][0] = true
+      else
+        Hydra.ingest_word(node[head], tail)
+      end
+    end
   end
 
   def ingest(words)
+    if words.is_a? Enumerable
+      words.each do |word|
+        ingest(word)
+      end
+    elsif words.is_a? String
+      Hydra.ingest_word(@root, words)
+    end
+  end
+
+  def dump
+    pp @root
   end
 end
--- spec/hydra_spec.rb
+++ spec/hydra_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
+# TODO Spec out .dump
 describe Hydra do
   describe '.count' do
-    it "counts" do
+    it "counts", focus: true do
       hydra = Hydra.new
       hydra.ingest(['a', 'b', 'c'])
+      expect(hydra.count).to eq 3
+    end
+  end
+
+  describe '.ingest' do
+    it "works with a single word" do
+      hydra = Hydra.new
+      hydra.ingest('bac')
+      expect(hydra.count).to eq 1
+    end
+
+    it "works with an array of words" do
+      hydra = Hydra.new
+      hydra.ingest(['democrat', 'democracy', 'democratic'])
       expect(hydra.count).to eq 3
     end
   end
