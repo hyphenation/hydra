73968f391013abfac0586b1680aed779cad59750
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2016-04-01 21:11:01 +0100

    *That* fails ...

--- spec/hydra_spec.rb
+++ spec/hydra_spec.rb
@@ -211,7 +211,7 @@ describe Pattern do
     it "works with patterns with trailing digits" do
       pattern = Pattern.new('ab2')
       expect(pattern).to be_a Pattern
-      expect(pattern.get_word).to [0, 0, 2]
+      expect(pattern.get_word).to eq [0, 0, 2]
       expect(pattern.get_digits).to eq 'ab'
     end
   end
