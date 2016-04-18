ecee032c1983a68923c81b35b5789bc9fb983b88
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2016-04-18 16:11:57 +0100

    Level 5.

--- spec/hydra_spec.rb
+++ spec/hydra_spec.rb
@@ -1320,6 +1320,11 @@ describe Heracles do
         hydra = Heracles.new.run_array(@complex_dictionary, [1, 4, 2, 5, 1, 1, 1, 2, 5, 1, 2, 1, 2, 6, 1, 1, 1, 2, 6, 1, 4, 1])
         expect(hydra.digest).to be == ['b1c', '1bcdex', '1bcx', '1bx', 'c1d', '2cdefx', '4defghx', '2dx', '1efghx', '1ex', 'f1g']
       end
+
+      it "generates level 5" do
+        hydra = Heracles.new.run_array(@complex_dictionary, [1, 5, 2, 5, 1, 1, 1, 2, 5, 1, 2, 1, 2, 6, 1, 1, 1, 2, 6, 1, 4, 1, 2, 7, 1, 1, 1])
+        expect(hydra.digest).to be == ['b1c', '1bcdex', '1bcx', '1bx', 'c1d', '2cdefx', '4defghx', '2dx', '1efghx', '1ex', 'f1g']
+      end
     end
   end
 
